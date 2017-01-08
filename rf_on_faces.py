5caf53196b72d48d3afcfcddd3f96aa503537b9f
ctiennot <c.tiennot@gmail.com>
2017-01-08 21:02:10 +0100

    minor changes

--- rf_on_faces.py
+++ rf_on_faces.py
@@ -4,6 +4,9 @@ from scipy import misc
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.cross_validation import train_test_split
+from spearman import score_function
+import time
 
 all_faces = {int(number[::-1][4:][::-1]): "data/extracted_faces/" + number
                   for number in os.listdir("data/extracted_faces")
@@ -24,3 +27,32 @@ for i in range(13000):
         X[i, :] = misc.imread(all_faces[i + 1]).reshape((-1,)) / 255.
 
 
+# read .csv using pandas
+y = pd.read_csv("data/training_outputs.csv", delimiter=";").set_index("ID")
+y = y.as_matrix().reshape((-1,))
+
+# store id's
+X_ids = range(1, 13001)
+
+# cut into test, train
+X_train, X_test, y_train, y_test = train_test_split(X[0:10000], y, test_size=0.3, random_state=42)
+
+# now we try the random forest
+rf = RandomForestRegressor(n_estimators=100, n_jobs=-1, verbose=True)
+
+start = time.time()
+rf.fit(X_train, y_train)
+print "Running time", round(time.time() - start), "s"
+
+# MSE on train and test
+print "train\t", round(rf.score(X_train, y_train), 2)
+print "test\t", round(rf.score(X_test, y_test), 2)
+
+preds = rf.predict(X_test)
+
+plt.figure()
+plt.plot(y_test, preds, "o")
+plt.plot([0,25], [0, 25])
+plt.show()
+
+score_function(y_pred=np.round(preds), y_true=y_test)

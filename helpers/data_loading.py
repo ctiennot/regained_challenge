c043e5c275ec098b775453aed8e33826e720afd9
ctiennot <c.tiennot@gmail.com>
2017-01-08 21:03:01 +0100

    added functions to get local and global views

--- helpers/data_loading.py
+++ helpers/data_loading.py
@@ -45,9 +45,10 @@ def load_labels(file="data/training_outputs.csv"):
     return y, ids
 
 
-def load_meta_features(file="data/facial_features_train.csv"):
+def load_meta_features(file="data/facial_features_train.csv", only_num=False):
     """
     :param file: "data/facial_features_train.csv" by default
+    :param only_num: if True then discard non numerical features
     :return:
     """
     # read .csv using pandas
@@ -74,6 +75,13 @@ def load_meta_features(file="data/facial_features_train.csv"):
                 "confidence_right_eye"]:
         X[col] = X[col].apply(lambda x: 0 if x == "None" else float(x))
 
+    if only_num:
+        numeric_features = np.array(
+            [type(value) is not str for value in X.as_matrix()[0, :]])
+        numeric_features = list(X.columns[numeric_features])
+        numeric_features.sort()
+        X = X[numeric_features]
+
     return X, ids
 
 
@@ -86,3 +94,149 @@ def export_submission(scores_sub, name="submission"):
     submission = pd.DataFrame(submission, columns=["ID", "TARGET"])
     name_sub = "submissions/" + name + time.strftime("_%d%m_%H%M") + ".csv"
     submission.to_csv(name_sub, sep=";", index=False)
+
+
+def global_views_batch(
+        pict_indices,
+        dir="data/global_views/", verbose=False, black_white=True):
+    """ Load faces images (in black and white)
+    :param pict_indices: list/array with indices of images to load in the batch
+    :param dir: "data/extracted_faces/" by default
+    :param black_white: if True converts RGB to black and white, if False then
+    for b&w images we replicate three times the grey channel.
+    :param verbose: display loading progression
+    :return: X, ids
+    """
+    all_img = {int(number[::-1][4:][::-1]): dir + number
+                      for number in os.listdir(dir)
+                      if number[::-1][:4] == 'gpj.'}
+
+    w, h = Image.open(all_img[1]).size
+
+    # initialize empty bashes
+    if black_white:
+        X = np.zeros((len(pict_indices), w, h), dtype=np.float32)
+    else:
+        X = np.zeros((len(pict_indices), w, h, 3), dtype=np.float32)
+
+    # fill them in
+    for i, index in enumerate(pict_indices):
+        if (i+1) % 100 == 0 and verbose:
+            print "{}/{} global views loaded".format(i+1, len(pict_indices))
+        # get image
+        im = Image.open(all_img[index])
+        # first case: we want to keep colors
+        if not black_white:
+            im = np.array(im).astype(np.float32)
+            # for color img
+            if len(im.shape)>2:
+                X[i] = im
+            # for B&W img we replicate three times
+            else:
+                X[i, :, :, 0] = im
+                X[i, :, :, 1] = im
+                X[i, :, :, 2] = im
+        # second case: we want B&W
+        else:
+            # for color img
+            if len(np.array(im).shape)>2:
+                im = im.convert('L')  # convert in B&W first
+                im = np.array(im).astype(np.float32)
+                X[i, :, :] = im
+            # for B&W img it's already good
+            else:
+                im = np.array(im).astype(np.float32)
+                X[i, :, :] = im
+
+    ids = np.array(pict_indices)
+    return X, ids
+
+
+def local_views_batch(
+        pict_indices,
+        dir="data/local_views/", verbose=False, black_white=True,
+        seed = None
+    ):
+    """ Load faces images (in black and white)
+    :param pict_indices: list/array with indices of images to load in the batch
+    it selects randomly 1 croped square among all those availables
+    :param dir: "data/extracted_faces/" by default
+    :param black_white: if True converts RGB to black and white, if False then
+    for b&w images we replicate three times the grey channel.
+    :param verbose: display loading progression
+    :param seed: if one, set seed
+    :return: X, ids
+    """
+    if seed is not None:
+        np.random.seed(seed)
+
+    def valid_file_name(name):
+        """ Returns only filenames that represent images"""
+        if not name.__contains__("jpg"):
+            return None  # not an image (can be .gitignore for instance)
+        # parse index and patch number
+        index, patch_number = tuple(map(int, name.split(".")[0].split("_")))
+        return index, patch_number
+
+    all_img = {}
+
+    for file_name in os.listdir(dir):
+        tup_index_patch = valid_file_name(file_name)
+        if tup_index_patch is None:
+            pass
+        else:
+            index, patch_number = tup_index_patch
+            if index in pict_indices:
+                if index in all_img.keys():
+                    all_img[index].append(patch_number)
+                else:
+                    all_img[index] = [patch_number]
+
+    # we sort the patch number lists to avoid messing up with the seed
+    for little_list in all_img.values():
+        little_list.sort()
+
+    # look at first img to know the dimensions
+    img_ = all_img.items()[0]
+    w, h = Image.open(dir + str(img_[0]) + '_' + str(img_[1][0]) + '.jpg').size
+
+    # initialize empty bashes
+    if black_white:
+        X = np.zeros((len(pict_indices), w, h), dtype=np.float32)
+    else:
+        X = np.zeros((len(pict_indices), w, h, 3), dtype=np.float32)
+
+    # fill them in
+    for i, index in enumerate(pict_indices):
+        if (i+1) % 100 == 0 and verbose:
+            print "{}/{} local views loaded".format(i+1, len(pict_indices))
+        # randomly sample a patch to load
+        k = np.random.choice(all_img[index], 1, False)[0]
+        name = dir + str(index) + '_' + str(k) + '.jpg'
+        # get image
+        im = Image.open(name)
+        # first case: we want to keep colors
+        if not black_white:
+            im = np.array(im).astype(np.float32)
+            # for color img
+            if len(im.shape)>2:
+                X[i] = im
+            # for B&W img we replicate three times
+            else:
+                X[i, :, :, 0] = im
+                X[i, :, :, 1] = im
+                X[i, :, :, 2] = im
+        # second case: we want B&W
+        else:
+            # for color img
+            if len(np.array(im).shape)>2:
+                im = im.convert('L')  # convert in B&W first
+                im = np.array(im).astype(np.float32)
+                X[i, :, :] = im
+            # for B&W img it's already good
+            else:
+                im = np.array(im).astype(np.float32)
+                X[i, :, :] = im
+
+    ids = np.array(pict_indices)
+    return X, ids
---
+++ helpers/img_processing.py
@@ -0,0 +1,118 @@
+"""
+This file is about processing the raw images:
+
+    - cropping some parts of them on demand
+    - resizing all images to the same size using center-crop, wrap or padding
+
+"""
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import pandas as pd
+import matplotlib.patches as patches
+
+
+def load_files_names():
+    pictures_train = {int(number[::-1][4:][::-1]): "data/pictures_train/" + number
+                      for number in os.listdir("data/pictures_train")
+                      if number[::-1][:4] == 'gpj.'}
+
+    pictures_test = {int(number[::-1][4:][::-1]): "data/pictures_test/" + number
+                      for number in os.listdir("data/pictures_test")
+                      if number[::-1][:4] == 'gpj.'}
+
+    return pictures_train, pictures_test
+
+
+def global_view(img, method='center-crop', resize=None):
+    """
+    Return a global view (square size) of the image using the method given
+    :param img: PIL.image object
+    :param method: 'center-crop', 'warp' or 'Padding'
+    :param resize: tuple, if provided resize the image to (w, h)
+    :return: image
+    """
+    if method == 'center-crop':
+
+        width, height = img.size   # Get dimensions
+        new_width = new_height = min(img.size)
+        left = (width - new_width)/2
+        top = (height - new_height)/2
+        right = (width + new_width)/2
+        bottom = (height + new_height)/2
+
+        img = img.crop((left, top, right, bottom))
+
+    if resize is not None:
+        img = img.resize(resize)
+
+    return img
+
+
+def export_all_global(method='center-crop', resize=(500, 500), verbose=True):
+    """Export all global views"""
+    # save all training
+    for i in range(1, 10001):
+        if verbose and i%500==0:
+            print "{}/{} views exported".format(i, 13000)
+        global_view(Image.open(pictures_train[i]), method, resize).save(
+            "data/global_views/{}.jpg".format(i))
+
+    # save all testing
+    for i in range(10001, 13001):
+        if verbose and i%500==0:
+            print "{}/{} views exported".format(i, 13000)
+        global_view(Image.open(pictures_test[i]), method, resize).save(
+            "data/global_views/{}.jpg".format(i))
+
+
+def local_view(img, patch=(200, 200)):
+    """ Return a randomly sampled patch from the image"""
+    w_patch, h_patch = patch
+    width, height = img.size
+    w_rand = int(np.random.rand()*(width-w_patch))  # random width start
+    h_rand = int(np.random.rand()*(height-h_patch))  # random height start
+    return img.crop((w_rand, h_rand, w_rand+w_patch, h_rand+h_patch))
+
+
+def export_all_local(method='center-crop', resize=(500, 500),
+                     patch_per_img=10, verbose=True, seed=None):
+    """
+    Export all local views. Names are "id_k.jpg" where id is the id of the
+    original image and k is the number of the patch (in 1:patch_per_img).
+    :param patch_per_img: number of patch to create per image
+    """
+    if seed is not None:
+        np.random.seed(seed)
+
+    # save all training
+    for i in range(1, 10001):
+        if verbose and i%500==0:
+            print "{}*{}/{} views exported".format(i, patch_per_img, 13000)
+        img = Image.open(pictures_train[i])
+        for k in range(patch_per_img):
+            local_view(img, patch=(200, 200)).save(
+                "data/local_views/{}_{}.jpg".format(i, k))
+
+    # save all testing
+    for i in range(10001, 13001):
+        if verbose and i%500==0:
+            print "{}*{}/{} views exported".format(i, patch_per_img, 13000)
+        img = Image.open(pictures_test[i])
+        for k in range(patch_per_img):
+            local_view(img, patch=(200, 200)).save(
+                "data/local_views/{}_{}.jpg".format(i, k))
+
+
+if __name__ == '__ main__':
+
+    # necessary global variables
+    pictures_train, pictures_test = load_files_names()
+
+    if False:
+        export_all_global()
+
+    if False:
+        export_all_local(patch_per_img=10, seed=1789)
